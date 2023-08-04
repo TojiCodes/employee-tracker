335f155b130ac28b2e09ff80124a15815d48f576
/\///\/_//\ _ <tojicodes@gmail.com>
2023-08-03 20:20:59 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,14 +6,14 @@ VALUES ('Sales'),
     ('Finance');
     
 INSERT INTO role (title, salary, department_id)
-VALUES ('Sales Lead', 100000, 1),
-    ('Sales Person', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('HR Manager', 90000, 3),
-    ('HR Assistant', 60000, 3),
-    ('Finance Manager', 110000, 4),
-    ('Accountant', 70000, 4);
+VALUES ('Sales Lead', 150000, 1),
+    ('Sales Person', 100000, 1),
+    ('Lead Engineer', 120000, 2),
+    ('Software Engineer', 100000, 2),
+    ('HR Manager', 70000, 3),
+    ('HR Assistant', 50000, 3),
+    ('Finance Manager', 250000, 4),
+    ('Accountant', 50000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL),
